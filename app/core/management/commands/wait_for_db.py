aa5e979fdb17cc20c469fe118894e16099cda9fe
igndukwe <ifeanyigloryndukwe@gmail.com>
2020-08-02 01:23:18 +1200

    Configured postgress db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available
+    """
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+        while not db_conn:
+            try:
+                # try and set db connection
+                db_conn = connections['default']
+            except OperationalError:
+                # output msg
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                # wait for 1 sec and try again
+                time.sleep(1)
+        # style.SUCCESS wraps it in a green output
+        self.stdout.write(self.style.SUCCESS('Database available!'))
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,29 @@
+# Simulate the db being avaliable or not
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+    # what happens when db is already avaliable
+    def test_wait_for_db_ready(self):
+        """Test waithing for db when db is available
+        """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')  # core/management/wait_for_db.py
+            # check that getitem was called once
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db
+        """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # the 1st 5 times will raise an operational error and 6th is a success
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')  # core/management/wait_for_db.py
+            # ckeck that getitem is called five times and sixth is a success
+            self.assertEqual(gi.call_count, 6)
